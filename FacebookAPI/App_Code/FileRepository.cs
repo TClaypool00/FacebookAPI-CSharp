265f81da29fb73e5f11f5ac115a43a4e93a7528c
claypooltyler00 <claypooltyler00@gmail.com>
2023-10-24 19:54:54 -0500

    File Repository, may not need this

---
+++ FacebookAPI/App_Code/FileRepository.cs
@@ -0,0 +1,53 @@
+﻿namespace FacebookAPI.App_Code
+{
+    public class FileRepository
+    {
+        public static string NewFileName(IFormFile file)
+        {
+            string justFileName = Path.GetFileNameWithoutExtension(file.FileName);
+            string fileExt = Path.GetExtension(file.FileName);
+            return $"{justFileName}-{Guid.NewGuid()}{fileExt}";
+        }
+
+        public static async Task CreateNewFileAsync(string fileName, IWebHostEnvironment environment, int userId, IFormFile file)
+        {
+            try
+            {
+                string userFolder = $@"{environment.WebRootPath}\Images\user{userId}\";
+                string filePath;
+
+                if (!Directory.Exists(userFolder))
+                {
+                    Directory.CreateDirectory(userFolder);
+                }
+
+                filePath = $"{userFolder}{fileName}";
+
+                var fileStream = new FileStream(filePath, FileMode.Create);
+                if (file.Length > 0)
+                {
+                    await file.CopyToAsync(fileStream);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static string GetFullUserPath(int userId, string fileName, IWebHostEnvironment environment)
+        {
+            return $@"{GetUserFolderPath(userId, environment)}\{fileName}";
+        }
+
+        public static string GetUserFolderPath(int userId, IWebHostEnvironment environment)
+        {
+            return $@"{environment.WebRootPath}\Images\user{userId}";
+        }
+
+        public static string GetUserPath(int userId, string fileName)
+        {
+            return $"user{userId}/{fileName}";
+        }
+    }
+}

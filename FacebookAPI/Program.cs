9a0d806328e49d9bbc86611404f02738ef820989
claypooltyler00 <claypooltyler00@gmail.com>
2023-10-24 19:56:19 -0500

    Added Connection string

--- FacebookAPI/Program.cs
+++ FacebookAPI/Program.cs
@@ -1,3 +1,7 @@
+using FacebookAPI;
+using FacebookAPI.App_Code.BOL;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Connection string
+builder.Services.AddDbContext<FacebookDbContext>(options => options.UseMySql(SecretConfig.ConnectionString, new MySqlServerVersion(SecretConfig.Version)));
 
 var app = builder.Build();
 

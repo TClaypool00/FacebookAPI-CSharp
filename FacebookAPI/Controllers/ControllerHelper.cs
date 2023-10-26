47b0e553808d536cc43d058d335383f536700e97
claypooltyler00 <claypooltyler00@gmail.com>
2023-10-25 22:11:14 -0500

    Added Controller helper class

---
+++ FacebookAPI/Controllers/ControllerHelper.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FacebookAPI.Controllers
+{
+    public class ControllerHelper : ControllerBase
+    {
+        #region Private fields
+        private readonly string _errorMessage;
+        protected readonly IConfiguration _configuration;
+        #endregion
+
+        #region Constructors
+        public ControllerHelper(IConfiguration configuration) : base()
+        {
+            _configuration = configuration;
+            _errorMessage = _configuration.GetSection("Messages").GetSection("Internal").Value;
+        }
+        #endregion
+
+        #region Protected methods
+        protected ActionResult InternalError(Exception exception)
+        {
+            return StatusCode(500);
+        }
+
+        protected ActionResult DisplayErrors()
+        {
+            var errorList = new List<string>();
+
+            foreach (var item in ModelState)
+            {
+                foreach (var error in item.Value.Errors)
+                {
+                    errorList.Add(error.ErrorMessage);
+                }
+            }
+
+            return BadRequest(errorList);
+        }
+        #endregion
+    }
+}

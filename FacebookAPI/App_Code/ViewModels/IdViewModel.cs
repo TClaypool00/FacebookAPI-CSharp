9f77faa4a590b3c5ba7fe451b1e5342d4f776bd3
TClaypool00 <claypooltyler00@gmail.com>
2023-11-03 21:58:57 -0500

    Added model just for the Id

---
+++ FacebookAPI/App_Code/ViewModels/IdViewModel.cs
@@ -0,0 +1,12 @@
+﻿using FacebookAPI.App_Code.CustomAnnotation;
+using System.ComponentModel.DataAnnotations;
+
+namespace FacebookAPI.App_Code.ViewModels
+{
+    public class IdViewModel
+    {
+        [Required(ErrorMessage = "Id is required")]
+        [IdMustBeGreaterThanZero]
+        public int Id { get; set; }
+    }
+}

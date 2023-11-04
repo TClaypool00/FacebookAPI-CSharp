91cab44adc814df2860896e0d925c8d45fc33edc
TClaypool00 <claypooltyler00@gmail.com>
2023-11-03 21:59:28 -0500

    Added a custom validation annotation to check if a number is greater than 0

---
+++ FacebookAPI/App_Code/CustomAnnotation/IdMustBeGreaterThanZero.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FacebookAPI.App_Code.CustomAnnotation
+{
+    public class IdMustBeGreaterThanZero : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value is int number)
+            {
+                if (number > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    ErrorMessage = "Id must be greater than 0";
+                    return false;
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Value must be an integer");
+            }
+        }
+    }
+}

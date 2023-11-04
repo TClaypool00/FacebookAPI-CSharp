aae525f1e04364d9ffc31a354eb0e9342d6f0cf9
claypooltyler00 <claypooltyler00@gmail.com>
2023-11-04 11:01:33 -0500

    Changed name of view model to PostLikeViewModel

--- FacebookAPI/App_Code/ViewModels/PostModels/PostLikeViewModel.cs
+++ FacebookAPI/App_Code/ViewModels/PostModels/PostLikeViewModel.cs
@@ -1,12 +1,15 @@
 ﻿using FacebookAPI.App_Code.CustomAnnotation;
 using System.ComponentModel.DataAnnotations;
 
-namespace FacebookAPI.App_Code.ViewModels
+namespace FacebookAPI.App_Code.ViewModels.PostModels
 {
-    public class IdViewModel
+    public class PostLikeViewModel
     {
         [Required(ErrorMessage = "Id is required")]
         [IdMustBeGreaterThanZero]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "User Id is required")]
+        public int UserId { get; set; }
     }
 }

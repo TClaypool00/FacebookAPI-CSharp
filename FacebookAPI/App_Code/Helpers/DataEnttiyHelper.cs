c535096adc8119806adf4418602fede4dd79750e
claypooltyler00 <claypooltyler00@gmail.com>
2023-11-05 07:55:44 -0600

    Created a helper class for all data entities

---
+++ FacebookAPI/App_Code/Helpers/DataEnttiyHelper.cs
@@ -0,0 +1,50 @@
+﻿using FacebookAPI.App_Code.BOL;
+
+namespace FacebookAPI.App_Code.Helpers
+{
+    public class DataEnttiyHelper
+    {
+        #region Private Fields
+        private List<int> _ids;
+        #endregion
+
+        #region Public Methods
+        public List<int> GetIds(List<Comment> comments)
+        {
+            _ids = new List<int>();
+
+            for (int i = 0; i < comments.Count; i++)
+            {
+                if (!Exists(comments[i].CommentId))
+                {
+                    _ids.Add(comments[i].CommentId);
+                }
+            }
+
+            return _ids;
+        }
+
+        public List<int> GetIds(List<Reply> replies)
+        {
+            _ids = new List<int>();
+
+            for (int i = 0; i < replies.Count; i++)
+            {
+                if (!Exists(replies[i].ReplyId))
+                {
+                    _ids.Add(replies[i].ReplyId);
+                }
+            }
+
+            return _ids;
+        }
+        #endregion
+
+        #region Private methods
+        private bool Exists(int id)
+        {
+            return _ids.Any(x => x == id);
+        }
+        #endregion
+    }
+}

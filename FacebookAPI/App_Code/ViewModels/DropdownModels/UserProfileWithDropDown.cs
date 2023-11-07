45f193ac5c67dc913b0590318f5d20fe2e95134e
claypooltyler00 <claypooltyler00@gmail.com>
2023-11-07 10:13:22 -0600

    Created model that includes drop down that inherites from UserProfileFullViewModel

---
+++ FacebookAPI/App_Code/ViewModels/DropdownModels/UserProfileWithDropDown.cs
@@ -0,0 +1,35 @@
+﻿using FacebookAPI.App_Code.CoreModels;
+using FacebookAPI.App_Code.ViewModels.FullModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace FacebookAPI.App_Code.ViewModels.DropdownModels
+{
+    public class UserProfileWithDropDown : FullUserProfileViewModel
+    {
+        #region Private Fields
+        private readonly List<SelectListItem> _genderDropDown;
+        #endregion
+
+        #region Constructors
+        public UserProfileWithDropDown()
+        {
+            
+        }
+
+        public UserProfileWithDropDown(CoreUser coreUser, List<SelectListItem> genderDropDown) : base(coreUser)
+        {
+            _genderDropDown = genderDropDown;
+        }
+        #endregion
+
+        #region Public Properties
+        public List<SelectListItem> GenderDropDown
+        {
+            get
+            {
+                return _genderDropDown;
+            }
+        }
+        #endregion
+    }
+}

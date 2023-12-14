8ccbf0d88cd6aae055a972aa848be378300f1c38
claypooltyler00 <claypooltyler00@gmail.com>
2023-12-13 20:51:44 -0600

    Created custom Model Builder

---
+++ FacebookAPI/App_Code/ModelBuilders/MetadataValueModelBinder.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Text.Json;
+
+namespace FacebookAPI.App_Code.ModelBuilders
+{
+    public class MetadataValueModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext == null)
+                throw new ArgumentNullException(nameof(bindingContext));
+
+            var values = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+
+            if (values.Length == 0)
+                return Task.CompletedTask;
+            var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+
+            var deserialized = JsonSerializer.Deserialize(values.FirstValue, bindingContext.ModelType, options);
+
+            bindingContext.Result = ModelBindingResult.Success(deserialized);
+            return Task.CompletedTask;
+        }
+    }
+}

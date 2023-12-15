a385b14440315e5613bca0d2490a836a1446d830
claypooltyler00 <claypooltyler00@gmail.com>
2023-12-14 23:05:11 -0600

    Added method to delete pictures

--- FacebookAPI/App_Code/Helpers/FileHelper.cs
+++ FacebookAPI/App_Code/Helpers/FileHelper.cs
@@ -7,15 +7,17 @@ namespace FacebookAPI.App_Code.Helpers
     public class FileHelper
     {
         private readonly IConfiguration _configuration;
+        private readonly string _notFoundMessage;
 
         public FileHelper(IConfiguration configuration)
         {
             _configuration = configuration;
+            _notFoundMessage = _configuration.GetSection("messages").GetSection("PictureNotFound").Value;
         }
 
         public async Task AddPicture(CorePicture corePicture)
         {
-            string userPath = $"{SecretConfig.DirectoryPath}\\{corePicture.UserFolderPath}";
+            string userPath = GetUserPath(corePicture.UserId);
 
             if (!Directory.Exists(userPath))
             {
@@ -34,8 +36,32 @@ namespace FacebookAPI.App_Code.Helpers
             }
             else
             {
-                throw new ApplicationException(_configuration.GetSection("messages").GetSection("PictureNotFound").Value);
+                throw new ApplicationException(_notFoundMessage);
             }
+        }
+
+        public void DeletePicture(Picture picture)
+        {
+            string picturePath = GetPicturePath(picture);
+
+            if (File.Exists(picturePath))
+            {
+                File.Delete(picturePath);
+            }
+            else
+            {
+                throw new ApplicationException(_notFoundMessage);
+            }
+        }
+
+        public string GetPicturePath(Picture picture)
+        {
+            return $@"{GetUserPath(picture.UserId)}\{picture.PictureFileName}";
+        }
+
+        private string GetUserPath(int userId)
+        {
+            return $@"{SecretConfig.DirectoryPath}\{_configuration.GetSection("tableNames").GetSection("User").Value}{userId}";
         }
     }
 }
